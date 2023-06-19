cf9ae8a84ddd8c43ae885ac8cba96b39b8be8bf6
jweaker <jweaker.t@gmail.com>
2023-06-19 14:03:57 +0300

    init

---
+++ configs/null-ls.lua
@@ -0,0 +1,39 @@
+local present, null_ls = pcall(require, "null-ls")
+
+if not present then
+  return
+end
+
+local b = null_ls.builtins
+
+local sources = {
+
+  -- webdev stuff
+  b.formatting.prettierd.with {
+    filetypes = { "html", "markdown", "css", "javascript", "javascriptreact", "typescript", "typesriptreact" },
+  }, -- so prettier works only on these filetypes
+  b.diagnostics.eslint_d.with { filetypes = { "javascript", "javascriptreact", "typescript", "typesriptreact" } },
+
+  -- Lua
+  b.formatting.stylua,
+
+  -- cpp
+  b.formatting.clang_format,
+}
+local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+null_ls.setup {
+  debug = true,
+  sources = sources,
+  on_attach = function(client, bufnr)
+    if client.supports_method "textDocument/formatting" then
+      vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+      vim.api.nvim_create_autocmd("BufWritePre", {
+        group = augroup,
+        buffer = bufnr,
+        callback = function()
+          vim.lsp.buf.format()
+        end,
+      })
+    end
+  end,
+}
